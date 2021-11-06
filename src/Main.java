21a529a70a92901ee18ca1853b15d2885b92cdf3
ModestVale <visible7@mail.ru>
2021-11-06 13:21:34 +0500

    Задача №1 - Мили

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+double ticketPrice = 4560.89;
+double milePrice = 20;
+int milesBalance = (int) (ticketPrice/milePrice);
+System.out.println("Miles balance: " + milesBalance);
+    }
+}
